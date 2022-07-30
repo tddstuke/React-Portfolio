6f2ec9f31f140dd1deddcd157d87af5598c4c889
Todd Stuke <tddstuke@gmail.com>
2022-07-30 11:26:16 -0500

    made projects cards anchors general cleanup on project module

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,32 +6,42 @@ function Project() {
       name: "Not Another IMDB",
       description:
         "Search for your favorite movies or find new favorites and add then to your custom lists!",
+      link: "https://protected-woodland-09194.herokuapp.com",
     },
     {
       name: "Tech Blog",
       description:
         "Read, Write and comment on Tech news using this intuitive blog site!",
+      link: "https://agile-mesa-60950.herokuapp.com/",
     },
   ]);
 
   return (
     <div className="row d-flex justify-content-around my-5">
       {projects.map((image, i) => (
-        <div className="card col-md-5  ">
+        <a
+          href={image.link}
+          className="card col-md-5 p-2 text-reset text-decoration-none"
+        >
           <div className="col">
-            <div className="card-block px-2">
+            <div className="card-block p-2">
               <h4 className="card-title">{image.name}</h4>
               <p className="card-text">{image.description}</p>
             </div>
+
+            <div className="row no-gutters">
+              {/* <div className="col-6 carousel-caption text-danger text-left">
+              <h4 className="card-title">{image.name}</h4>
+              <p className=" card-text">{image.description}</p>
+            </div> */}
+              <img
+                className="col-md-12"
+                src={require(`../../assets/${i}.jpg`)}
+                alt={image.name}
+              />
+            </div>
           </div>
-          <div className="row no-gutters">
-            <img
-              className="col-md-12"
-              src={require(`../../assets/${i}.jpg`)}
-              alt={image.name}
-            />
-          </div>
-        </div>
+        </a>
       ))}
     </div>
   );
