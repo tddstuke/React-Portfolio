89d700e4d634935749d350c8223556ac12513a4a
Todd Stuke <tddstuke@gmail.com>
2022-07-30 08:00:14 -0500

    added href links to footer icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,16 +3,20 @@ import React from "react";
 function footer() {
   return (
     <div className="container">
-      <footer className="row">
-        <p className="col-md-1">
+      <footer className="row d-flex justify-content-center">
+        <a href="https://github.com/tddstuke" className="col-md-1">
           <i className="bi bi-github" style={{ fontSize: 50 }}></i>
-        </p>
-        <p className="col-md-1">
+        </a>
+        <a
+          href="https://linkedin.com/in/todd-stuke
+"
+          className="col-md-1"
+        >
           <i className="bi bi-linkedin" style={{ fontSize: 50 }}></i>
-        </p>
-        <p className="col-md-1">
+        </a>
+        <a href="https://twitter.com/tddstuke" className="col-md-1">
           <i className="bi bi-twitter" style={{ fontSize: 50 }}></i>
-        </p>
+        </a>
       </footer>
     </div>
   );
