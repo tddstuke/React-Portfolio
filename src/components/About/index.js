24d89a2037567ce19596b3bb3351645416965fac
Todd Stuke <tddstuke@gmail.com>
2022-09-04 09:59:40 -0500

    more responsiveness tweeks

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,14 +4,17 @@ function About() {
   return (
     <div className="container">
       <section>
-        <h3 className="m-5 text-center text-lg-start">About Me</h3>
-        <div className="row">
-          <img
-            className="mx-3 rounded-circle col-md-6 col-lg-3"
-            src={require("../../assets/me.jpg")}
-            alt="my profile"
-          />
-          <div className="col-md-6 col-lg-8">
+        <h3 className="m-5 text-center text-lg-start px-5">About Me</h3>
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-4 col-lg-3 d-flex justify-content-md-center">
+            <img
+              className="rounded-circle mx-auto"
+              style={{ height: "180px" }}
+              src={require("../../assets/me.jpg")}
+              alt="my profile"
+            />
+          </div>
+          <div className="col-md-12 col-lg-8">
             <p className="m-5">
               {" "}
               Hello! My name is Todd Stuke and I am a recent graduate of a 6
