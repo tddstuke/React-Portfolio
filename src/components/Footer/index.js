fba191aa9ed3735c922d38da0641fc9bcadf0885
Todd Stuke <tddstuke@gmail.com>
2022-09-04 14:02:37 -0500

    footer refractor for responsiveness

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,17 +4,17 @@ function footer() {
   return (
     <div className="container mt-5">
       <footer className="row d-flex justify-content-center">
-        <a href="https://github.com/tddstuke" className="col-md-1">
+        <a href="https://github.com/tddstuke" className="col-1">
           <i className="bi bi-github" style={{ fontSize: 50 }}></i>
         </a>
         <a
           href="https://linkedin.com/in/todd-stuke
 "
-          className="col-md-1"
+          className="col-1 mx-5"
         >
           <i className="bi bi-linkedin" style={{ fontSize: 50 }}></i>
         </a>
-        <a href="https://twitter.com/tddstuke" className="col-md-1">
+        <a href="https://twitter.com/tddstuke" className="col-1 me-5">
           <i className="bi bi-twitter" style={{ fontSize: 50 }}></i>
         </a>
       </footer>
