7f270b64c7068418dc24c08042d206bd5b347c0f
Todd Stuke <tddstuke@gmail.com>
2022-09-04 10:34:30 -0500

    making about me  a card

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,28 +5,30 @@ function About() {
     <div className="container">
       <section>
         <h3 className="m-5 text-center text-lg-start px-5">About Me</h3>
-        <div className="row justify-content-center align-items-center">
-          <div className="col-md-4 col-lg-3 d-flex justify-content-md-center">
-            <img
-              className="rounded-circle mx-auto"
-              style={{ height: "180px" }}
-              src={require("../../assets/me.jpg")}
-              alt="my profile"
-            />
-          </div>
-          <div className="col-md-12 col-lg-8">
-            <p className="m-5">
-              {" "}
-              Hello! My name is Todd Stuke and I am a recent graduate of a 6
-              month full-stack coding bootcamp at the University of Kansas.
-              Prior to this I had no coding or web development experience.
-              Through hard work and determination I have now developed skills in
-              both front-end and back-end web development. I hope you'll take
-              time to look at my projects I built during class (including this
-              portfolio!) and some I have built or modified after. My Resume and
-              a Contact page are also available here as well as a link to my
-              GitHub. Hope to hear from you soon!
-            </p>
+        <div className="card">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-3 col-lg-3 d-flex justify-content-md-center">
+              <img
+                className="rounded-circle m-auto my-5"
+                style={{ height: "180px" }}
+                src={require("../../assets/me.jpg")}
+                alt="my profile"
+              />
+            </div>
+            <div className="col-md-12 col-lg-8">
+              <p className="m-5">
+                {" "}
+                Hello! My name is Todd Stuke and I am a recent graduate of a 6
+                month full-stack coding bootcamp at the University of Kansas.
+                Prior to this I had no coding or web development experience.
+                Through hard work and determination I have now developed skills
+                in both front-end and back-end web development. I hope you'll
+                take time to look at my projects I built during class (including
+                this portfolio!) and some I have built or modified after. My
+                Resume and a Contact page are also available here as well as a
+                link to my GitHub. Hope to hear from you soon!
+              </p>
+            </div>
           </div>
         </div>
       </section>
