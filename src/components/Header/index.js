69cc92d77470411230909cecef6bc36f3df704ce
Todd Stuke <tddstuke@gmail.com>
2022-09-04 22:25:03 -0500

    changed title and icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,20 +5,10 @@ function Header(props) {
   const { navChoices = [], setCurrentChoice, currentChoice } = props;
   return (
     <div className="container">
-      {/* <header className="row d-md-flex justify-content-md-between"> */}
-      {/* <h2 className="col-md-3 mt-5 mb-3">
-          <a
-            className="text-reset text-decoration-none"
-            data-testid="link"
-            href="/"
-          >
-            Todd Stuke
-          </a>
-        </h2> */}
       <Navbar expand="md" className=" row d-md-flex justify-content-md-between">
         <Container>
           <Navbar.Brand
-            className="navbar-brand fw-bold fs-2 col-md-3 mt-md-5 mb-md-3 mx-sm-3 ml-sm-0"
+            className="navbar-brand fw-bold fs-1 col-md-3 mt-md-5 mb-md-3 mx-sm-3"
             href="/"
           >
             Todd Stuke
